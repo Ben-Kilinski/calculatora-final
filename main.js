493b5536eac5b47397285421ad001a32f5066145
Ben-Kilinski <ben.kilinski@gmail.com>
2023-09-12 16:50:40 +0300

    calculadora

---
+++ main.js
@@ -0,0 +1,25 @@
+const screenDisplay = document.querySelector(".screen")
+const buttons = document.querySelectorAll("button")
+
+let calculation = []
+let accumulativeCalculation
+
+function calculate(button) {
+    const digit = button.textContent
+    
+    if (digit == "CLEAR"){
+        calculation = []
+        screenDisplay.textContent = "."
+    }
+    else if (digit == "="){
+        console.log(accumulativeCalculation)
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    }
+    else {
+        calculation.push(digit)
+        accumulativeCalculation = calculation.join(" ")
+        screenDisplay.textContent = accumulativeCalculation
+    }
+}
+
+buttons.forEach(button => button.addEventListener("click", () => calculate(button)))
